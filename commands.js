06b543e3d286cb2264e08a1d769b9d10e0d9700b
K1dfun <125171801+K1dfun@users.noreply.github.com>
2025-12-15 00:29:13 +0000

    thanks chatgpt

--- commands.js
+++ commands.js
@@ -1,16 +1,16 @@
 export const commands = [
-    {
-        name: "queue",
-        description: "check if your level is in the verifier queue",
-        options: [
-            {
-                name: "level url",
-                description: "",
-                required: true,
-                type: 3
-            }
-        ],
-        integration_types: [0, 1],
-        contexts: [0, 1, 2]
-    }
+  {
+    name: "queue",
+    description: "Check if your level is in the verifier queue",
+    options: [
+      {
+        name: "level_url", 
+        description: "GRAB level URL",
+        required: true,
+        type: 3
+      }
+    ],
+    integration_types: [0, 1],
+    contexts: [0, 1, 2]
+  }
 ];
