fec63a025a3fc54650c6e45c3316b0062351e3e6
K1dfun <125171801+K1dfun@users.noreply.github.com>
2025-12-15 23:56:59 +0000

    risky update

--- commands.js
+++ commands.js
@@ -5,7 +5,7 @@ export const commands = [
     options: [
       {
         name: "level_url",
-        description: "grab level url",
+        description: "level url",
         type: 3,
         required: true
       }
@@ -20,7 +20,28 @@ export const commands = [
     options: [
       {
         name: "level_url",
-        description: "grab level url",
+        description: "level url",
+        type: 3,
+        required: true
+      }
+    ],
+    integration_types: [0, 1],
+    contexts: [0, 1, 2]
+  },
+
+  {
+    name: "leaderboard_search",
+    description: "search for a user's record",
+    options: [
+      {
+        name: "username",
+        description: "user name",
+        type: 3,
+        required: true
+      },
+      {
+        name: "level_url",
+        description: "level url",
         type: 3,
         required: true
       }
