b4378b779b42d461dcccb556e78b6790e43b20af
K1dfun <125171801+K1dfun@users.noreply.github.com>
2025-12-15 02:01:30 +0000

    new command support

--- commands.js
+++ commands.js
@@ -1,13 +1,28 @@
 export const commands = [
   {
     name: "queue",
-    description: "Check if your level is in the verifier queue",
+    description: "check if a level is in the verifier queue",
     options: [
       {
-        name: "level_url", 
-        description: "GRAB level URL",
-        required: true,
-        type: 3
+        name: "level_url",
+        description: "grab level url",
+        type: 3,
+        required: true
+      }
+    ],
+    integration_types: [0, 1],
+    contexts: [0, 1, 2]
+  },
+
+  {
+    name: "publish_time",
+    description: "view the publish time for a level",
+    options: [
+      {
+        name: "level_url",
+        description: "grab level url",
+        type: 3,
+        required: true
       }
     ],
     integration_types: [0, 1],
